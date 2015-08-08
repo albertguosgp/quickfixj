a30a3fca30d666834b7b114d38225f1b25ff7c5f
Guo chen <albertguosgp@gmail.com>
2015-08-08 09:46:28 +0800

    test

--- quickfixj-core/src/main/java/org/quickfixj/Version.java
+++ quickfixj-core/src/main/java/org/quickfixj/Version.java
@@ -20,8 +20,7 @@
 package org.quickfixj;
 
 public class Version {
-
- public static void main(String[] args) {
+public static void main(String[] args) {
         System.out.println(getVersion());
     }
 
