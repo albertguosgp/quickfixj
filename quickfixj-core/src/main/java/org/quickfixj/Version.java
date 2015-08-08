4053759e35854ece03d1d7f093c68f5c5b65ff62
Chen Guo <chenguo@Chens-MacBook-Pro.local>
2015-08-08 09:42:42 +0800

    auto format

--- quickfixj-core/src/main/java/org/quickfixj/Version.java
+++ quickfixj-core/src/main/java/org/quickfixj/Version.java
@@ -20,7 +20,8 @@
 package org.quickfixj;
 
 public class Version {
-    public static void main(String[] args) {
+
+public static void main(String[] args) {
         System.out.println(getVersion());
     }
 
